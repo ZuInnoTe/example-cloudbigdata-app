3c73bed681ac921d678a28e2b7c5e0b63a9be9b7
Jörn Franke <jornfranke@gmail.com>
2014-04-07 23:40:59 +0200

    Upgrade to newest version of spring boot

--- src/main/java/org/zuinnote/cloudbigdata/config/WebSocketConfig.java
+++ src/main/java/org/zuinnote/cloudbigdata/config/WebSocketConfig.java
@@ -32,12 +32,14 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+
 
 import org.zuinnote.cloudbigdata.configmanager.ConfigManagerInterface;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 @Autowired
 private ConfigManagerInterface configManager;
@@ -53,12 +55,7 @@ private ConfigManagerInterface configManager;
 		registry.addEndpoint("/cloudbigdata/ws").withSockJS();
 	}
 
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration channelRegistration) {
-	}
 
-	@Override
-	public void configureClientOutboundChannel(ChannelRegistration channelRegistration) {
-	}
+
 
 }
